6cdd0c82e16e77e4989b5a7263e2d672578a72e1
Erfan Ansari <dev.erfanansari@gmail.com>
2025-12-17 13:57:43 +0330

    feat: add vazir font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,26 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Vazirmatn } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
+  display: 'swap',
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  weight: ['300', '400', '500', '600', '700'],
 });
 
 const geistMono = Geist_Mono({
+  display: 'swap',
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  weight: ['300', '400', '500', '600', '700'],
+});
+
+const persianFont = Vazirmatn({
+  display: 'swap',
+  variable: "--font-persian",
+  subsets: ["arabic"],
+  weight: ['300', '400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${persianFont.variable} antialiased`}
       >
         {children}
       </body>
